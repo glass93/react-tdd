aa742fe7b18a5e6e5422b2b26882b098611b787c
3jung <hsj7630@daum.net>
2021-07-20 20:16:14 +0900

    chore: const person

--- jest-test/index.js
+++ jest-test/index.js
@@ -2,6 +2,14 @@ const sum = (a, b) => {
   return a + b;
 };
 
+const person = (name, age) => {
+  return {
+    name,
+    age,
+  };
+};
+
 module.exports = {
   sum,
+  person,
 };
--- jest-test/index.test.js
+++ jest-test/index.test.js
@@ -1,7 +1,13 @@
-const { sum } = require("./index");
+const { sum, person } = require("./index");
 
 describe("test index.js file", () => {
   it("sums 1 + 2 to equal 3", () => {
     expect(sum(1, 2)).toBe(3);
+  });
+  it("makes a person", () => {
+    expect(person("Kim", 20)).toEqual({
+      name: "Kim",
+      age: 20,
+    });
   });
 });
